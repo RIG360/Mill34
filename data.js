4e6e81591d9ef0a01da0c4b3f2012e031690a0a5
RIG360 <134787168+RIG360@users.noreply.github.com>
2023-05-27 16:49:26 +0200

    upload

---
+++ data.js
@@ -0,0 +1,533 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-eingangstr-haus-1",
+      "name": "Eingangstür Haus 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.11741137062577067,
+        "pitch": 0.02864902588025231,
+        "fov": 2.0943319123931956
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04521451725349124,
+          "pitch": -0.0548364165698203,
+          "rotation": 0,
+          "target": "1-vorraum-eg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-vorraum-eg",
+      "name": "Vorraum EG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.7657323793026354,
+        "pitch": 0.017304581739217895,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.023768726816495445,
+          "pitch": -0.039709637327515424,
+          "rotation": 0,
+          "target": "2-atelier"
+        },
+        {
+          "yaw": -1.544894057604676,
+          "pitch": -0.03431605637438295,
+          "rotation": 0,
+          "target": "4-stiegeantritt-eg"
+        },
+        {
+          "yaw": 1.4185009765449124,
+          "pitch": 0.017613994591620497,
+          "rotation": 0,
+          "target": "0-eingangstr-haus-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-atelier",
+      "name": "Atelier",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -1.5870581667319161,
+        "pitch": -0.014726216125474423,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.399181433819143,
+          "pitch": 0.059403046607707566,
+          "rotation": 0,
+          "target": "3-bad-atelier"
+        },
+        {
+          "yaw": 1.5666921539752536,
+          "pitch": 0.08416922746524591,
+          "rotation": 0,
+          "target": "1-vorraum-eg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bad-atelier",
+      "name": "Bad (Atelier)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -1.4849646318054432,
+        "pitch": 0.06856943025969287,
+        "fov": 1.5775163724593209
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4313860470880364,
+          "pitch": 0.09158506658523535,
+          "rotation": 0,
+          "target": "2-atelier"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-stiegeantritt-eg",
+      "name": "Stiegeantritt EG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0.70812159119399,
+        "pitch": 0.007202589544466065,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03987895174827827,
+          "pitch": -0.024912814979742848,
+          "rotation": 0,
+          "target": "5-stiegenaustritt-1og"
+        },
+        {
+          "yaw": 1.4405679871628738,
+          "pitch": 0.5001526032843273,
+          "rotation": 0,
+          "target": "1-vorraum-eg"
+        },
+        {
+          "yaw": 1.4354475332061334,
+          "pitch": 0.07780831539483657,
+          "rotation": 0,
+          "target": "0-eingangstr-haus-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-stiegenaustritt-1og",
+      "name": "Stiegenaustritt 1.OG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 2.6554943622531777,
+        "pitch": 0.09357971121907305,
+        "fov": 1.9726509926463358
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0929790362261116,
+          "pitch": 0.5943952074903649,
+          "rotation": 0,
+          "target": "4-stiegeantritt-eg"
+        },
+        {
+          "yaw": 2.03704122717775,
+          "pitch": 0.04177571870348196,
+          "rotation": 0,
+          "target": "6-wohnkche"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-wohnkche",
+      "name": "Wohnküche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 1.9118404962331077,
+        "pitch": 0.005175201431125487,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1082258620278331,
+          "pitch": 0.10284771769114087,
+          "rotation": 0,
+          "target": "5-stiegenaustritt-1og"
+        },
+        {
+          "yaw": -2.6033857418372577,
+          "pitch": 0.01662989905273271,
+          "rotation": 0,
+          "target": "7-stiegenantritt-1og"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-stiegenantritt-1og",
+      "name": "Stiegenantritt 1.OG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0.5443910229206317,
+        "pitch": 0.0058757967336511285,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1126660373379416,
+          "pitch": 0.03437176009597742,
+          "rotation": 0,
+          "target": "6-wohnkche"
+        },
+        {
+          "yaw": -0.10515542414553991,
+          "pitch": 0.03475662349349662,
+          "rotation": 0,
+          "target": "8-vorraum-2og"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-vorraum-2og",
+      "name": "Vorraum 2.OG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0.058036253413597194,
+        "pitch": 0.07673138146298086,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9375453428760885,
+          "pitch": 0.41929523595094054,
+          "rotation": 0,
+          "target": "7-stiegenantritt-1og"
+        },
+        {
+          "yaw": -1.0595576044654749,
+          "pitch": 0.1954423814052575,
+          "rotation": 0,
+          "target": "11-schrankraum"
+        },
+        {
+          "yaw": 0.009798006704851758,
+          "pitch": 0.12226488016453985,
+          "rotation": 0,
+          "target": "10-bad"
+        },
+        {
+          "yaw": 1.3575535817238595,
+          "pitch": 0.13946216483003937,
+          "rotation": 0,
+          "target": "9-zimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-zimmer",
+      "name": "Zimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 1.5536453176979403,
+        "pitch": -0.0013043925698390524,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.33677042685088665,
+          "pitch": 0.1190986627932098,
+          "rotation": 0,
+          "target": "8-vorraum-2og"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-bad",
+      "name": "Bad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 1.6874517756090635,
+        "pitch": -0.0009976357668151081,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.000546239146548,
+          "pitch": 0.04835227294706179,
+          "rotation": 0,
+          "target": "8-vorraum-2og"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-schrankraum",
+      "name": "Schrankraum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.06964347814112415,
+        "pitch": 0.0016373537357310397,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.152107506453362,
+          "pitch": 0.06352637528527438,
+          "rotation": 0,
+          "target": "8-vorraum-2og"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mill34, Haus 1, 230508",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
